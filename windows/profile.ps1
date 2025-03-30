d68f1b0a4cde538c43bff69179857e7f298c16a8
Eric Moore <EricGittaMoore@duck.com>
2025-03-31 02:13:48 +0800

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -2,8 +2,6 @@
 
 Set-Alias -Name l -Value Get-ChildItem
 
-Invoke-Expression (& { (lua C:\Users\admin\scoop\apps\z.lua\current\z.lua --init powershell echo once enhanced) -join "`n" })
-
 if (Test-Path $env:ChocolateyInstall\helpers\chocolateyProfile.psm1) {
     Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 }
@@ -20,3 +18,6 @@ function CondaInit {
 # 执行下面，不然 conda 的 env 会加载 starship 前面
 # conda config --set changeps1 false
 Invoke-Expression (&starship init powershell)
+
+# 必须要在 starship 后面，不然不生效
+Invoke-Expression (& { (lua C:\Users\admin\scoop\apps\z.lua\current\z.lua --init powershell echo once enhanced) -join "`n" })
