c80da06997a4cab5239273a759c7d916e5b306a9
Eric Moore <EricGittaMoore@duck.com>
2025-03-31 00:19:23 +0800

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -6,13 +6,24 @@ if (Test-Path $env:ChocolateyInstall\helpers\chocolateyProfile.psm1) {
     Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 }
 
-
+# conda config --set auto_activate_base false
+# conda config --set changeps1 false
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe") {
-    (& "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+function Use-Conda {
+    param (
+        [string] $Activate
+    )
+    
+    $CondaExe = "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe"
+    If (Test-Path $CondaExe) {
+        (& $CondaExe "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+        
+        if ($Activate) {
+            conda activate $Activate
+        }
+    }
 }
 #endregion
-
 
 Invoke-Expression (&starship init powershell)
