f97c3ae0fbf5e9523e5251dcc568662c9dfed45d
Eric Moore <EricGittaMoore@duck.com>
2025-03-31 02:43:01 +0800

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,17 +1,11 @@
 # 全部都放在 $PROFILE.CurrentUserAllHosts 文件里面就 ok
 
-Set-Alias -Name l -Value Get-ChildItem
-
-if (Test-Path $env:ChocolateyInstall\helpers\chocolateyProfile.psm1) {
-    Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-}
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 function CondaInit {
-    If (Test-Path "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe") {
-        (& "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-    }
+    (& "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
 }
 #endregion
 
@@ -21,3 +15,9 @@ Invoke-Expression (&starship init powershell)
 
 # 必须要在 starship 后面，不然不生效
 Invoke-Expression (& { (lua C:\Users\admin\scoop\apps\z.lua\current\z.lua --init powershell echo once enhanced) -join "`n" })
+
+# alias
+Set-Alias -Name l -Value Get-ChildItem
+function gitc { 
+    git config user.email "EricGittaMoore@duck.com"; git config user.name "Eric Moore"
+}
