f2be3124b2255c6eaf3f37df5d184176a82d1435
Eric Moore <EricGittaMoore@duck.com>
2025-04-01 12:04:46 +0800

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -6,9 +6,7 @@ Import-Module syntax-highlighting
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
-function CondaInit {
-    (& "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
+function CondaInit { (& "$HOME\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression }
 #endregion
 
 # 执行下面，不然 conda 的 env 会加载 starship 前面
@@ -16,26 +14,11 @@ function CondaInit {
 Invoke-Expression (&starship init powershell)
 
 # 必须要在 starship 后面，不然不生效
-Invoke-Expression (& { (lua C:\Users\admin\scoop\apps\z.lua\current\z.lua --init powershell echo once enhanced) -join "`n" })
+Invoke-Expression (& { (lua $HOME\scoop\apps\z.lua\current\z.lua --init powershell echo once enhanced) -join "`n" })
 
 # alias
 Set-Alias -Name l -Value Get-ChildItem
-function gitc { 
-    git config user.email "EricGittaMoore@duck.com"; git config user.name "Eric Moore"
-}
+function gitc { git config user.email "EricGittaMoore@duck.com"; git config user.name "Eric Moore" }
 # https://stackoverflow.com/questions/29266622/how-to-run-exe-with-without-elevated-privileges-from-powershell
-function isAdministrator {
-    $CurrentID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $CurrentPrincipal = new-object System.Security.Principal.WindowsPrincipal($CurrentID)
-    $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-
-    # Check to see if session is currently with admin privileges
-    if ($CurrentPrincipal.IsInRole($adminRole)) {
-        write-host "Yes we are running elevated."
-    }else{
-        write-host "No this is a normal user session."
-    }
-}
-function toGeneralUser {
-    runas /trustlevel:0x20000 pwsh
-}
+function isAdministrator { echo $(if ((new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) { "Yes we are running elevated." } else { "No this is a normal user session." }) }
+function toGeneralUser { runas /trustlevel:0x20000 pwsh }
